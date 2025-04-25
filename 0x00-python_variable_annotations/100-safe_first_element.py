cb4e511acf099e806d908bd0eab305ec00414556
nhonoree <honoreniyirema@gmail.com>
2025-04-25 23:00:32 +0000

    all tasks

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-from typing import Sequence, Any, Union
+"""Duck typing - first element of a sequence"""
+
+from typing import Any, Sequence, Union
+
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Returns the first element of the sequence if it exists, otherwise None"""
     if lst:
         return lst[0]
-    else:
-        return None
+    return None
